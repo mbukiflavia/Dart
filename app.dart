3223b85593f5d1c877aaa0d69182e2af27d2056b
FlaviaMbuki <mbukiflavia@gmail.com>
2022-11-18 15:06:00 +0300

    Dart data types

---
+++ app.dart
@@ -0,0 +1,28 @@
+void main() {
+  //declaring a variable of type Integer
+  int currentYear = 2022;
+
+  //declaring a variable of type String
+  String myName = "Flavia";
+
+  //declaring a variable of type Boolean
+  bool isItChristmas = false;
+
+  //declaring a variable of type List
+  List friends = ["Chandler", "Monica", "Rachel"];
+
+  //printing an item in a list
+  print(friends[0]);
+
+  //adding an item to the list
+  friends.add("Sheldon Cooper");
+
+  //deleting an item from a list
+  friends.remove("Monica");
+
+  //print variables.
+  print(currentYear);
+  print(myName);
+  print(isItChristmas);
+  print(friends);
+}
