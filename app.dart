5ea820c75a6592a6655f141e669617f7dff763ca
FlaviaMbuki <mbukiflavia@gmail.com>
2022-11-19 22:51:17 +0300

    Dart data types

--- app.dart
+++ app.dart
@@ -20,9 +20,38 @@ void main() {
   //deleting an item from a list
   friends.remove("Monica");
 
+  Map user = {
+    "firstName": "Sameen Shaw",
+    "age": 45,
+    "Occupation": "Dart instructor",
+  };
+
+  var heartSymbol = '\u2665';
+  var laughEmoji = '\u{1f600}';
+
   //print variables.
   print(currentYear);
   print(myName);
   print(isItChristmas);
   print(friends);
+
+  //print user
+  print(user);
+
+  //print an item from a map
+  print(user['firstName']);
+  print(user['age']);
+
+  //adding an item to a map
+  user["Country"] = "Zambia";
+
+  //print all items in a map
+  print(user);
+
+  //remove/delete an item from a map
+  user.remove("age");
+  print(user);
+
+  print(heartSymbol);
+  print(laughEmoji);
 }
