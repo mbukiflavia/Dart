39af99851451ed9de759d7ff0f733227312a6136
FlaviaMbuki <mbukiflavia@gmail.com>
2022-11-21 12:34:35 +0300

    functions

--- functions.dart
+++ functions.dart
@@ -1,1 +1,19 @@
-void main() {}
+//void is a return type that doesn't return anything.
+
+// returnType functionName(parameter 1, parameter 2, ...){
+// return value;
+// }
+
+//no parameter & no return type function.
+
+void main() {
+  printFullName();
+}
+
+void printFullName() {
+  print('I am groot');
+}
+
+void printFullNames(String name) {
+  print('My name is $name');
+}
